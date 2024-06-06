64f9d074d224f7d4017323b4f0ce9a81320e7662
Albino-Fox <albinofox9@gmail.com>
2024-06-06 12:31:52 +1000

    designed 404

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-card class="pd-3">
+  <v-card class="pa-10 d-flex align-center flex-column">
     <h1>4o4</h1>
     <!-- <p>Do not the {{ this.$route.params.pathMatch[0] }}</p> -->
     <p>Whatcha doing here?</p>
-    <AppLink to="/">Go home</AppLink>
+    <AppLink to="/"><p class="border-lg pa-1 mt-3 rounded">Go home</p></AppLink>
   </v-card>
 </template>
