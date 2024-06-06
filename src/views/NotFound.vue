b2bc77e58d54aa5afb705d940d27b3747412ce9f
Albino-Fox <albinofox9@gmail.com>
2024-06-06 12:58:31 +1000

    changed type of container

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
-  <v-card class="pa-10 d-flex align-center flex-column">
+  <div class="pa-10 d-flex align-center flex-column">
     <h1>4o4</h1>
     <!-- <p>Do not the {{ this.$route.params.pathMatch[0] }}</p> -->
     <p>Whatcha doing here?</p>
     <AppLink to="/"><p class="border-lg pa-1 mt-3 rounded">Go home</p></AppLink>
-  </v-card>
+  </div>
 </template>
